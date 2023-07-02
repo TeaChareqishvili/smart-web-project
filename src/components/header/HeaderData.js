42c868b011172cf16f93125ac3c32e43b69b6abf
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-07-02 21:01:16 +0400

    header

---
+++ src/components/header/HeaderData.js
@@ -0,0 +1,49 @@
+
+import personIcon from '../../assets/Vector.png';
+import messageIcon from "../../assets/message.png";
+import heartIcon from '../../assets/heart.png';
+import cardIcon from '../../assets/busket.png';
+
+const icons = [
+    {
+        icon:personIcon,
+        title:'Profile'
+    },
+    {
+       icon:messageIcon,
+       title: 'Message'
+    },
+    { 
+        icon:heartIcon,
+        title:"Orders"
+    },
+    {
+        icon:cardIcon,
+        title:'My cart'
+    }
+];
+
+export {icons};
+
+const categories = [
+    {
+        title:"All category"
+    },
+    {
+        title:"Hot offers"
+    },
+    {
+        title:"Gift boxes"
+    },
+    {
+        title:"Projects"
+    },
+    {
+        title:"Menu item"
+    },
+    {
+        title:"Help"
+    },
+];
+
+export {categories}
