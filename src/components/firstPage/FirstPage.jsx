fedd45b16ace88487a50fa0ce3beef5666b814eb
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-07-06 01:18:42 +0400

    region suppliers

--- src/components/firstPage/FirstPage.jsx
+++ src/components/firstPage/FirstPage.jsx
@@ -1,22 +1,28 @@
 import { DealAndOffers } from "../dealAndOffers/DealAndOffers";
-import { Banner } from "../firstPageBanner/Banner"
- import { ElectorinicsSource } from "../sourceNow/ElectronicsSource";
- import { HomeAndOutdoor } from "../sourceNow/HomeAndOutdoor";
+import { Banner } from "../firstPageBanner/Banner";
+import { ElectorinicsSource } from "../sourceNow/ElectronicsSource";
+import { HomeAndOutdoor } from "../sourceNow/HomeAndOutdoor";
 import { Subscribe } from "../subscribe/Subscribe";
- import { Suppliers } from "../suppliers/Suppliers";
+import { Suppliers } from "../suppliers/Suppliers";
+import { Regions } from "../suppliersRegion/Regions";
 import "./Style.scss";
 
-const FirstPage = ()=>{
-    return (
-        <div className="main-wrapper">
-            <Banner/>
-             <DealAndOffers/> 
-             <HomeAndOutdoor/>
-            <ElectorinicsSource/>
-            <Suppliers/> 
-            <Subscribe/>
-        </div>
-    )
-}
+const FirstPage = () => {
+  return (
+    <>
+      <div className="main-wrapper">
+        <Banner />
+        <DealAndOffers />
+        <HomeAndOutdoor />
+        <ElectorinicsSource />
+        <Suppliers />
+        <Regions />
+      </div>
+      <div>
+        <Subscribe />
+      </div>
+    </>
+  );
+};
 
-export {FirstPage}
+export { FirstPage };
