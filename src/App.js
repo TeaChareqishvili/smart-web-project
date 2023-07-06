4c2a12ae4ac05b082713a697e400ca92002c91d2
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-07-06 13:21:17 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Routes, Route } from "react-router-dom";
-import { FirstPage } from "./components/firstPage/FirstPage"; import { Footer } from "./components/footer/Footer";
+
+import { FirstPage } from "./components/firstPage/FirstPage"; 
+import { Footer } from "./components/footer/Footer";
 import { Header } from "./components/header/Header";
 
 
@@ -7,11 +8,7 @@ function App() {
   return (
     <div className="App">
      <Header/>
-      <Routes>
-        <Route path="/" element={<FirstPage />} />
-        {/* <Route path="/products" element={<Products />}>
-		<Route path="/admin/dashboard" element={<Dashboard />}> */}
-      </Routes>
+      <FirstPage/>
       <Footer/>
     </div>
   );
