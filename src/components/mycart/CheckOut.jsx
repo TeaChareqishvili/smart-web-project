367acc49cf7e19abfaa973d7e9dcba7c3f7c3a87
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-08-07 20:45:44 +0400

    changes

--- src/components/mycart/CheckOut.jsx
+++ src/components/mycart/CheckOut.jsx
@@ -4,47 +4,55 @@ import bank from "../../assets/Logo.svg";
 import P from "../../assets/Logo1.svg";
 import visa from "../../assets/Logo2.svg";
 import pay from "../../assets/image 21.png";
+import useHeartIconClick from "../hook/saveFavorite";
 
 const CheckOut = () => {
+
+  const { chosenItems } = useHeartIconClick();
+  if (chosenItems.length === 0) {
+    return null;
+  }
+
   return (
     <div className="main-checkout-wrapper">
-      <div className="coupon">
-        <p>Have a coupon?</p>
-        <div>
-          <input type="text" placeholder="Add coupon" />
-          <button>Apply</button>
-        </div>
-      </div>
-      <div className="checkout-wrapper">
-        <div className="coupon-checkout">
-          <div>
-            <p>Subtotal:</p>
-            <p>Discoun:t</p>
-            <p>Tax:</p>
-          </div>
-          <div>
-            <span className="grey">$1403.97</span>
-            <span className="red">-$60.00</span>
-            <span className="green">+$14.00</span>
-          </div>
-        </div>
-        <div className="border"></div>
-        <div className="checkout">
-          <div>
-            <h6>Total:</h6>
-            <h4>$1357.97</h4>
-          </div>
-          <button>Checkout</button>
-        </div>
-        <div className="pay-icons">
-          <img src={express} alt="pay-logo" />
-          <img src={bank} alt="pay-logo" />
-          <img src={P} alt="pay-logo" />
-          <img src={visa} alt="pay-logo" />
-          <img src={pay} alt="pay-logo" />
-        </div>
+    <div className="coupon">
+      <p>Have a coupon?</p>
+      <div>
+        <input type="text" placeholder="Add coupon" />
+        <button>Apply</button>
       </div>
     </div>
+    <div className="checkout-wrapper">
+      <div className="coupon-checkout">
+        <div>
+          <p>Subtotal:</p>
+          <p>Discoun:t</p>
+          <p>Tax:</p>
+        </div>
+        <div>
+          <span className="grey">$1403.97</span>
+          <span className="red">-$60.00</span>
+          <span className="green">+$14.00</span>
+        </div>
+      </div>
+      <div className="border"></div>
+      <div className="checkout">
+        <div>
+          <h6>Total:</h6>
+          <h4>$1357.97</h4>
+        </div>
+        <button>Checkout</button>
+      </div>
+      <div className="pay-icons">
+        <img src={express} alt="pay-logo" />
+        <img src={bank} alt="pay-logo" />
+        <img src={P} alt="pay-logo" />
+        <img src={visa} alt="pay-logo" />
+        <img src={pay} alt="pay-logo" />
+      </div>
+    </div>
+  </div>
+  
   );
 };
 export { CheckOut };
